a5d55a3f47e9bf6de04f366c40d1deb9338504d9
keshava-consumatrix-01 <keshava-consumatrix-01@users.noreply.github.com>
2022-01-11 13:39:37 +0100

    updated user agents

--- pararius.py
+++ pararius.py
@@ -5,38 +5,42 @@ from typing import Any, Dict, List
 import time
 import mysql.connector
 import ssl
-import dotenv
-ssl._create_default_https_context = ssl._create_unverified_context
+from tqdm import tqdm
 
+ssl._create_default_https_context = ssl._create_unverified_context
+import random
 import requests
 
-from fake_useragent import UserAgent
 
 from twilio.rest import Client
 
 from dotenv import load_dotenv
 from pathlib import Path
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
 
 
+# you can also import SoftwareEngine, HardwareType, SoftwareType, Popularity from random_user_agent.params
+# you can also set number of user agents required by providing `limit` as parameter
 
+software_names = [SoftwareName.EDGE.value,]
+operating_systems = [OperatingSystem.MAC.value,]
+
+user_agent_rotator = UserAgent(limit=10000)
+# Get list of user agents.
+user_agents = user_agent_rotator.get_user_agents()
+user_agent_list = [user['user_agent'] for user in user_agents]
+
+rex_url = re.compile(r'href="/(?:huis|appartement)-te-(?:koop|huur)/[^"]+')
 
 
 rex_json = re.compile(
-        re.escape('<script type="application/ld+json">') + '(.+)' + re.escape('</script>'),
-        re.DOTALL,
-    )
-ua = UserAgent()
-ua_final = ua.random
+    re.escape('<script type="application/ld+json">') + '(.+)' + re.escape('</script>'),
+    re.DOTALL,
+)
+
 ROOT_URL = 'https://www.pararius.nl'
-HEADERS = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    # noqa
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
-    'cache-control': 'max-age=0',
-    'referer': ROOT_URL,
-    'user-agent': ua_final,  # noqa
-}
+
 
 
 def sql_connection():
@@ -65,10 +69,7 @@ def select_table():
 
 
 def get_urls(url: str) -> List[str]:
-    ua = UserAgent()
-    ua_final = ua.random
-    rex_url = re.compile(r'href="/(?:huis|appartement)-te-(?:koop|huur)/[^"]+')
-    ROOT_URL = 'https://www.pararius.nl'
+    ua_final = random.choice(user_agent_list)
     HEADERS = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         # noqa
@@ -84,14 +85,25 @@ def get_urls(url: str) -> List[str]:
 
 
 def get_info(url: str) -> Dict[str, Any]:
-
+    ua_final_info = random.choice(user_agent_list)
+    # print(ua_final_info)
+    HEADERS_NEW = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,ru;q=0.8',
+        'cache-control': 'max-age=0',
+        'referer': ROOT_URL,
+        'user-agent': ua_final_info,  # noqa
+    }
     if not url.startswith(ROOT_URL):
         raise ValueError('invalid URL')
-    r = requests.get(url, headers=HEADERS)
+    r = requests.get(url, headers=HEADERS_NEW)
     r.raise_for_status()
+    # print(r.text)
     match = rex_json.search(r.text)
     if not match:
         print(url)
+        # print(r.text)
         raise ValueError('cannot find JSON data on page')
 
     raw = match.group(1)
@@ -104,14 +116,15 @@ def pararius_get_urls_list():
         urls_raw = get_urls(search_url)
         urls = list(set(urls_raw))
         time.sleep(3)
+        print(len(urls))
     return urls
 
 def pararius_get_data(urls):
     result_list = []
-    for url in urls:
+    for url in tqdm(urls):
         try:
             info = get_info(url)
-            if int(info['floorSize']['value']) >= 75:
+            if int(info['floorSize']['value']) >= 0:
                 result_dic = {
                     "url" : info['@id'],
                     "type" : info["@type"][0],
@@ -160,12 +173,13 @@ def main():
     # urls = pararius_get_urls_list()
     urls_sql = select_table()
     urls_pararius = pararius_get_urls_list()
-    results = pararius_get_data(urls_pararius)
-    urls_final = [result[0] for result in results if result[0] not in urls_sql]
-    result_final = [result for result in results if result[0] not in urls_sql]
-    insert_result_in_table(result_final)
-    if urls_final != []:
-        whatsapp_message(urls_final)
+    urls_to_scrape = [url for url in urls_pararius if url not in urls_sql]
+    results = pararius_get_data(urls_to_scrape)
+    # urls_final = [result[0] for result in results if result[0] not in urls_sql]
+    insert_result_in_table(results)
+    whatsapp_url = [result[0] for result in results if int(result[6]) >= 75]
+    if whatsapp_url != []:
+        whatsapp_message(whatsapp_url)
 
 if __name__ == "__main__":
     main()
